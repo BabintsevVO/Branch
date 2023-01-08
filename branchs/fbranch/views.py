309d9922fbaf08a05311984ccbb8cf7a984013fb
BabintsevVO <babintsevvo@gmail.com>
2023-01-08 11:17:00 +0300

    add info about 2 branch

--- branchs/fbranch/views.py
+++ branchs/fbranch/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>hii</h1>')
+    return HttpResponse('<h1>hii. now we have 2 branchs</h1>')
 
 # Create your views here.
